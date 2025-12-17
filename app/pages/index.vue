7c3a00bd781dbd7a9caa65136ad239e14501a3cb
kirano-dev <developer@kirano.uz>
2025-12-17 15:34:40 +0500

    !

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-6 container-fluid">
     <h1 class="text-3xl font-bold">Главная страница</h1>
-    <p class="mt-2">Стартовый Nuxt шаблон KIRANO.</p>
+    <p class="mt-2">Стартовый Nuxt шаблон KIRANO2.</p>
     <IconUser />
   </div>
 </template>
